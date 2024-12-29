bec4d8c682f0a371bf9bbed8ef922833efb22ea1
nikol <nikol.lazarte@540deg.com>
2024-12-30 00:09:04 +0100

    agregar zustand

---
+++ src/ui/hooks/usePokemonStore.tsx
@@ -0,0 +1,32 @@
+
+import { create } from "zustand";
+import { PokemonDetail } from "../../domain/entities/PokemonDetail";
+
+interface StoreState {
+  savedPokemons: PokemonDetail[];
+  addPokemon: (pokemon: PokemonDetail) => void;
+  updatePokemon: (pokemon: PokemonDetail) => void;
+}
+
+export const usePokemonStore = create<StoreState>()((set, get) => ({
+  savedPokemons: [],
+
+  addPokemon: (pokemon: PokemonDetail) => {
+    const savedPokemons = get().savedPokemons;
+    if (!savedPokemons.some(p => p.id === pokemon.id)) {
+      savedPokemons.push(pokemon);
+    }
+
+    set({ savedPokemons: [...savedPokemons] });
+  },
+
+  updatePokemon: (pokemon: PokemonDetail) => {
+    const savedPokemons = get().savedPokemons;
+
+    const updatedPokemons = savedPokemons.map(p =>
+      p.id === pokemon.id ? { ...p, ...pokemon } : p
+    );
+
+    set({ savedPokemons: updatedPokemons });
+  }
+}));
